56541049a7afee6a1b1079f9aca027e83082594c
hdog2n <tahsin.do1791@gmail.com>
2026-02-26 16:41:57 +0100

    add animal class

---
+++ shelter-frontend/src/app/class/animalClass.ts
@@ -0,0 +1,8 @@
+export interface Animal {
+    Id_Animal: number,
+    birthdate: string,
+    animal_name: string,
+    Id_race: number | null,
+    Id_Person: number | null,
+    race_name : string | null
+}
